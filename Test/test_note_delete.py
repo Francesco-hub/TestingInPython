f7580d752f2a899c7b2fe960c3241d58b6a50c6a
Francesco-hub <56824952+Francesco-hub@users.noreply.github.com>
2022-12-28 16:44:42 +0100

    Test Suite tuned

--- Test/test_note_delete.py
+++ Test/test_note_delete.py
@@ -16,22 +16,23 @@ class NotesDeleteUnitTests(unittest.TestCase):
         self.email = 'testingUser@test.com'
         self.password = '1234567Test'
 
+    def test_delete_note_failure(self):
+        self.app.post('/login', data={'email': self.email, 'password': self.password})
+        notes = self.app.get('/u')
+        self.assertEqual(b'Test note' in notes.data, True)
+        result = self.app.post('/delete-note', data=json.dumps({'noteId': 4}))
+        notes = self.app.get('/u')
+        self.assertEqual(b'Test note' in notes.data, True)
 
     def test_delete_note_success(self):
         self.app.post('/login', data={'email': self.email, 'password': self.password})
-        self.app.get('/u')
+        notes = self.app.get('/u')
+        self.assertEqual(b'Test note' in notes.data, True)
         result = self.app.post('/delete-note', data=(json.dumps({'noteId': self.note_id})))
         notes = self.app.get('/u')
         self.assertEqual(b'Test note' in notes.data, False)
 
-    def test_delete_note_failure(self):
-        '''# Send a POST request to the delete_note view with a valid note ID
-        result = self.app.post('/delete-note', data=json.dumps({'noteId': self.note_id}))
 
-        # Assert that the note was not deleted from the database
-        note = Note.query.get(self.note_id)
-        self.assertIsNotNone(note)'''
-        self.assertEqual(1,1)
 
 if __name__ == '__main__':
     unittest.main()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Website import create_app
 
-app = create_app('testing')
+app = create_app('Release')
 if __name__ == '__main__':
     app.run(debug=True)
