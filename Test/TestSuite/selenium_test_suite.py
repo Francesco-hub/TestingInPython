8abc9d0c70a73295b8df9be29c63e0f37c68e763
Francesco-hub <56824952+Francesco-hub@users.noreply.github.com>
2022-12-29 12:46:08 +0100

    Divided Tests into two suites

---
+++ Test/TestSuite/selenium_test_suite.py
@@ -0,0 +1,19 @@
+import unittest
+
+from Test.SeleniumTests.test_sign_up_selenium import SignUpUnitTestsSelenium
+from Test.SeleniumTests.test_authentication_selenium import AuthenticationUnitTestsSelenium
+from Test.SeleniumTests.test_notes_creation_selenium import NotesUnitTestsSelenium
+from Test.SeleniumTests.test_notes_delete_selenium import NotesDeleteUnitTestsSelenium
+
+unittest.TestLoader.sortTestMethodsUsing = None
+
+if __name__ == '__main__':
+
+    suite = unittest.TestSuite()
+    suite.addTests(SignUpUnitTestsSelenium)
+    suite.addTests(AuthenticationUnitTestsSelenium)
+    suite.addTests(NotesUnitTestsSelenium)
+    suite.addTests(NotesDeleteUnitTestsSelenium)
+
+
+    unittest.TextTestRunner.run(suite)
